f213f5ea6b0e832ec858747803c7fa8d29088b08
Theofanis Tokas <tokastheofanis@gmail.com>
2015-05-14 17:53:33 +0300

    Added draft post list, publish and delete button in post detail and implemented those actions.

--- broders/blog/views.py
+++ broders/blog/views.py
@@ -8,11 +8,11 @@ from .forms import PostForm
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
-	
+
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
-	
+
 def post_new(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
@@ -24,7 +24,7 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
-	
+
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == 'POST':
@@ -37,4 +37,17 @@ def post_edit(request, pk):
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
-	
+
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.delete()
+	return redirect('blog.views.post_list')
